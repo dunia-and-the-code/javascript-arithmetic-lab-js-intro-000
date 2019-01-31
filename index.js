8b2e68b2e9b23fbe2aeb1fb29e8f0fa663410117
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-31 15:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,10 +28,8 @@ function makeInt(n) {
 return parseInt('n', 10);
 }
 
-console.log(makeInt(0.2328, 10))
-function makeInt(n) {
-  return parseInt('0.2328', 10);
-}
+console.log(makeInt(0.2328, 10));
+
 
 function preserveDecimal(n) {
 return parseFloat('2.222');
